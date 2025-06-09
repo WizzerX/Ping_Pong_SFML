a1e5040cfcb2e41fee00390ab25b61cdf65bc551
Vpox7 <148094617+WizzerX@users.noreply.github.com>
2025-06-09 15:37:20 +0530

    bounce back function

--- SFML_GAME/SFML_GAME.cpp
+++ SFML_GAME/SFML_GAME.cpp
@@ -1,77 +1,84 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 1440, 1080 }), "Software Runing!");
-    sf::RectangleShape shape(sf::Vector2f(100.f,100.f));
-    shape.setFillColor(sf::Color::Cyan);
-   
-    sf::Texture bgtexture("D:/WebDeve/SFML/SFML_Projects/SFML_GAME/SFML_GAME/assets/images/background.jpg");
-    sf::Sprite bgsprite(bgtexture);
-    
-
-    sf::Vector2u windowsize = window.getSize();
-    sf::Vector2u texturesize = bgtexture.getSize();
-
-    float scalex = static_cast<float>(windowsize.x) / texturesize.y;
-    float scaley = static_cast<float>(windowsize.y) / texturesize.y;
-
-    bgsprite.setScale(sf::Vector2f(scalex, scaley));
-
-    sf::Texture texture1;
-    if (!texture1.loadFromFile("D:/WebDeve/SFML/SFML_Projects/SFML_GAME/SFML_GAME/SFML_GAME/football.jpg")) {
-        std::cout<<"ERROR IN LOADING THE FILE!n\n";
-    }
-
-    sf::RectangleShape shape3(sf::Vector2f(120.f,120.f));
-    shape3.setTexture(&texture1);
-    shape3.setPosition(sf::Vector2f(500.f, 800.f));
+	sf::RenderWindow Window(sf::VideoMode({ 1080,720 }), "GameRuning!");
+	//paw setup
+	sf::Texture PawTexture;
+	if (!PawTexture.loadFromFile("../assets/images/paw.png")) {
+		std::cout << "error in loading the pawtexture!\n";
+	}
+	sf::Sprite pawsprite(PawTexture);
+	pawsprite.setScale(sf::Vector2f(0.5f, 0.5f));
+	pawsprite.setPosition(sf::Vector2f(500.f, 1200.f));
 
 
+	Window.setMouseCursorVisible(false);
+	
+	sf::Texture backgroundtexture;
+	if (!backgroundtexture.loadFromFile("D:/WebDeve/SFML/SFML_Projects/SFML_GAME/SFML_GAME/assets/images/background.jpg")) {
+		std::cout << "Error in loading the background!\n";
+	}
 
-    sf::Texture sqauretexture("../assets/images/redball.jpg");
-    sf::Sprite sqauresprite(sqauretexture);
-    sqauresprite.setScale(sf::Vector2f(0.5f, 0.5f));
-    sqauresprite.setPosition(sf::Vector2f(500.f, 200.f));
-    
+	///background setup
+	sf::Sprite backgroundsprite(backgroundtexture);
 
+	sf::Vector2u backgroundsize = Window.getSize();
+	sf::Vector2u texturesize = backgroundtexture.getSize();
 
+	float scalex = static_cast<float>(backgroundsize.x) / texturesize.x;
+	float scaley = static_cast<float>(backgroundsize.y) / texturesize.y;
 
-  
-    
+	backgroundsprite.setScale(sf::Vector2f(scalex, scaley));
 
-    
-
-   
-
-   
+	sf::Vector2f ballVelocity(0.f, 0.5f); // Ball starts moving down
+	//ball setup
+	sf::Texture balltexture;
+	if (!balltexture.loadFromFile("../assets/images/orangeball.png")) {
+		std::cout << "error in load the ball texture!\n";
+	}
+	sf::Sprite ballsprite(balltexture);
+	ballsprite.setScale(sf::Vector2f(0.5f, 0.5f));
+	
 
 
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
 
 
-        shape.move(sf::Vector2f(0.f, 0.1f));
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
-            sf::Vector2i mousepos= sf::Mouse::getPosition(window);
-            shape.setPosition(sf::Vector2f(mousepos));
-        }
-        sqauresprite.move(sf::Vector2f(0.0f, 0.1f));
-       
 
-        window.clear();
-        window.draw(bgsprite);
-        window.draw(shape3);
-       // window.draw(square);
-        window.draw(sqauresprite);
+	while (Window.isOpen()) {
 
-        window.display();
-    }
-}
+
+		while (std::optional event = Window.pollEvent()) {
+			if (event->is<sf::Event::Closed>()) {
+				Window.close();
+			}
+			sf::Vector2i pos = sf::Mouse::getPosition(Window);
+			float  posx = static_cast<float>(pos.x);
+			//float  posy = static_cast<float>(pos.y);
+
+			pawsprite.setPosition(sf::Vector2f(posx,510.f));
+
+
+		}
+		sf::FloatRect boundingpaw = pawsprite.getGlobalBounds();
+		sf::FloatRect boundingball = ballsprite.getGlobalBounds();
+		if (boundingball.findIntersection(boundingpaw)) {
+			ballsprite.move(sf::Vector2f(0.0f, -0.5f));
+			std::cout << "YES!\n";
+		}else
+		ballsprite.move(sf::Vector2f(0.f, 0.2f));
+	
+
+		Window.clear();
+		Window.draw(backgroundsprite);
+		Window.draw(pawsprite);
+		Window.draw(ballsprite);
+		Window.display();
+	}
+
+
+
+};
